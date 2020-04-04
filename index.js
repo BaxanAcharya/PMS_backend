7c349d8bd1de990aeb0cecf87f084f534422d3ce
BaxanAcharya <baxanacharya@gmail.com>
2020-04-04 16:54:54 +0545

    connect to mongodb server

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express=require('express');
 const dotenv=require('dotenv').config()
+const mongoose=require('mongoose');
 const app=express();
 
-
+//connect to the mongodb database using the defined URL parameter
+mongoose.connect(process.env.URL,{useNewUrlParser:true,useUnifiedTopology:true, useFindAndModify:false,useCreateIndex:true})
+.then((db)=>{
+    console.log("Successfully connected to Mongodb Server")
+},(err)=>console.log(err));
 
 //to start the backend at defined port no
 app.listen(process.env.PORT,()=>{
