4b185ecc8170549ee91a4872e6adf065cc2ca607
BaxanAcharya <baxanacharya@gmail.com>
2020-04-04 17:52:29 +0545

    login api added

--- routes/user.js
+++ routes/user.js
@@ -35,4 +35,35 @@ route.post('/register',(req,res,next) =>{
     .catch(next)
 });
 
+
+route.post('/login',(req,res,next)=>{
+
+    User.findOne({username:req.body.username})
+    .then((user)=>{
+        if(user==null)
+        {
+            res.status(401)
+            res.json({code:401,status:'User not registered'})
+        }else if(user.verify==false)
+        {
+            res.status(402)
+            res.json({code:402, status:'Your account is not verified'})
+        }
+        else{
+            bcrypt.compare(req.body.password,user.password)
+            .then((isMatch)=>{
+                if(!isMatch){
+                    res.status(403)
+            res.json({code:403,status:"Password not valid"})
+                }
+                let token=jwttoken.sign({_id: user._id}, process.env.SECRET);
+                res.status(200);
+                res.json({code:200,status:'You have logged!!!', token:token});
+
+            }).catch(next);
+        }
+    }).catch(next);
+});
+
+
 module.exports = route;
