6eb14b45ba6735734a227b1925d5a49af46df0d8
BaxanAcharya <baxanacharya@gmail.com>
2020-04-04 17:11:57 +0545

    added required dependency and configured it

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express=require('express');
 const dotenv=require('dotenv').config()
 const mongoose=require('mongoose');
+const morgan=require('morgan');
+const cors=require('cors');
+
 const app=express();
+
+//for cross origin request
+app.use(cors());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+ "/public"))
+app.use(express.json());
+//for api hit logs
+app.use(morgan('tiny'));
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 //connect to the mongodb database using the defined URL parameter
 mongoose.connect(process.env.URL,{useNewUrlParser:true,useUnifiedTopology:true, useFindAndModify:false,useCreateIndex:true})
